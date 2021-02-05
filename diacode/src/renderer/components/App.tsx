fbf8c98d01663abd88c75d06e89a6f6043ac8fce
yatiyr <dereeren@yahoo.com>
2021-02-05 18:57:27 +0300

    initialized diacode

---
+++ diacode/src/renderer/components/App.tsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+import {hot} from "react-hot-loader/root";
+
+export default hot((): JSX.Element => 
+    (
+        <div>
+          <h1>
+            Your Electron App
+          </h1>
+        </div>
+    ));
